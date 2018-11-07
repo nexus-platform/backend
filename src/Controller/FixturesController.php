4f088ceee1661ce184098a95c4e27307c8a8fddc
fdbatista <fdbatista@gmail.com>
2018-11-07 16:15:53 -0500

    FixturesController

--- src/Controller/FixturesController.php
+++ src/Controller/FixturesController.php
@@ -247,10 +247,28 @@ class FixturesController extends Controller {
                         $acu->setUser($user);
                         $entityManager->persist($acu);
                     }
-                }
+                }   
             }
         }
         $entityManager->flush();
+        
+        StaticMembers::executeRawSQL($entityManager, 'insert into `ea_settings` (`id_assessment_center`, `name`, `value`) select distinct `ac_id`, "company_working_plan" as `name`, \'{"sunday":{"start":"09:00","end":"18:00","breaks":[{"start":"11:20","end":"11:30"},{"start":"14:30","end":"15:00"}]},"monday":{"start":"09:00","end":"18:00","breaks":[{"start":"11:20","end":"11:30"},{"start":"14:30","end":"15:00"}]},"tuesday":{"start":"09:00","end":"18:00","breaks":[{"start":"11:20","end":"11:30"},{"start":"14:30","end":"15:00"}]},"wednesday":{"start":"09:00","end":"18:00","breaks":[{"start":"11:20","end":"11:30"},{"start":"14:30","end":"15:00"}]},"thursday":{"start":"09:00","end":"18:00","breaks":[{"start":"11:20","end":"11:30"},{"start":"14:30","end":"15:00"}]},"friday":{"start":"09:00","end":"18:00","breaks":[{"start":"11:20","end":"11:30"},{"start":"14:30","end":"15:00"}]},"saturday":{"start":"09:00","end":"18:00","breaks":[{"start":"11:20","end":"11:30"},{"start":"14:30","end":"15:00"}]}}\' as `value` from `assessment_center_user` where `is_admin` = 1', false);
+        StaticMembers::executeRawSQL($entityManager, 'insert into `ea_settings` (`id_assessment_center`, `name`, `value`) select distinct `ac_id`, "book_advance_timeout" as `name`, "30" as `value` from `assessment_center_user` where `is_admin` = 1', false);
+        StaticMembers::executeRawSQL($entityManager, 'insert into `ea_settings` (`id_assessment_center`, `name`, `value`) select distinct `ac_id`, "company_name" as `name`, (select `name` from `assessment_center` where `id` = `acu`.`ac_id`) as `value` from `assessment_center_user` `acu` where `is_admin` = 1', false);
+        StaticMembers::executeRawSQL($entityManager, 'insert into `ea_settings` (`id_assessment_center`, `name`, `value`) select distinct `ac_id`, "google_analytics_code" as `name`, "" as `value` from `assessment_center_user` where `is_admin` = 1', false);
+        StaticMembers::executeRawSQL($entityManager, 'insert into `ea_settings` (`id_assessment_center`, `name`, `value`) select distinct `ac_id`, "company_email" as `name`, (select email from user where id = `acu`.`user_id`) as `value` from `assessment_center_user` `acu` where `is_admin` = 1', false);
+        StaticMembers::executeRawSQL($entityManager, 'insert into `ea_settings` (`id_assessment_center`, `name`, `value`) select distinct `ac_id`, "company_link" as `name`, (select `url` from `assessment_center` where `id` = `acu`.`ac_id`) as `value` from `assessment_center_user` `acu` where `is_admin` = 1', false);
+        StaticMembers::executeRawSQL($entityManager, 'insert into `ea_settings` (`id_assessment_center`, `name`, `value`) select distinct `ac_id`, "customer_notifications" as `name`, "1" as `value` from `assessment_center_user` where `is_admin` = 1', false);
+        StaticMembers::executeRawSQL($entityManager, 'insert into `ea_settings` (`id_assessment_center`, `name`, `value`) select distinct `ac_id`, "date_format" as `name`, "DMY" as `value` from `assessment_center_user` where `is_admin` = 1', false);
+        StaticMembers::executeRawSQL($entityManager, 'insert into `ea_settings` (`id_assessment_center`, `name`, `value`) select distinct `ac_id`, "time_format" as `name`, "regular" as `value` from `assessment_center_user` where `is_admin` = 1', false);
+        StaticMembers::executeRawSQL($entityManager, 'insert into `ea_settings` (`id_assessment_center`, `name`, `value`) select distinct `ac_id`, "require_captcha" as `name`, "0" as `value` from `assessment_center_user` where `is_admin` = 1', false);
+        StaticMembers::executeRawSQL($entityManager, 'insert into `ea_settings` (`id_assessment_center`, `name`, `value`) select distinct `ac_id`, "display_cookie_notice" as `name`, "0" as `value` from `assessment_center_user` where `is_admin` = 1', false);
+        StaticMembers::executeRawSQL($entityManager, 'insert into `ea_settings` (`id_assessment_center`, `name`, `value`) select distinct `ac_id`, "cookie_notice_content" as `name`, "Cookie notice content." as `value` from `assessment_center_user` where `is_admin` = 1', false);
+        StaticMembers::executeRawSQL($entityManager, 'insert into `ea_settings` (`id_assessment_center`, `name`, `value`) select distinct `ac_id`, "display_terms_and_conditions" as `name`, "0" as `value` from `assessment_center_user` where `is_admin` = 1', false);
+        StaticMembers::executeRawSQL($entityManager, 'insert into `ea_settings` (`id_assessment_center`, `name`, `value`) select distinct `ac_id`, "terms_and_conditions_content" as `name`, "Terms and conditions content." as `value` from `assessment_center_user` where `is_admin` = 1', false);
+        StaticMembers::executeRawSQL($entityManager, 'insert into `ea_settings` (`id_assessment_center`, `name`, `value`) select distinct `ac_id`, "display_privacy_policy" as `name`, "0" as `value` from `assessment_center_user` where `is_admin` = 1', false);
+        StaticMembers::executeRawSQL($entityManager, 'insert into `ea_settings` (`id_assessment_center`, `name`, `value`) select distinct `ac_id`, "privacy_policy_content" as `name`, "Privacy policy content." as `value` from `assessment_center_user` where `is_admin` = 1', false);
+        
         return $count;
     }
 
